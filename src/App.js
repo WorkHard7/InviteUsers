51d083fb6b2ce11bb5be8bbd4137ee17e62a7ef6
Mihu <solinuxnet@gmail.com>
2022-09-19 15:26:14 +0300

    'first commit'

--- src/App.js
+++ src/App.js
@@ -1,15 +1,61 @@
-import React from 'react';
-import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import React from "react";
+import { useState, useEffect } from "react";
+import "./index.scss";
+import { Success } from "./components/Success";
+import { Users } from "./components/Users";
 
-// Тут список пользователей: https://reqres.in/api/users
+// Here is user lists: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setLoading] = useState([true]);
+  const [searchValue, setSearchValue] = useState("");
+  const [success, setSucces] = useState(false);
+  const [invites, setInvites] = useState([]);
+
+  useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      })
+      .catch((err) => {
+        console.warn(err);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  const onChangeSearchValue = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites((prev) => prev.filter((_id) => _id !== id)); // if finds this id in our array(invites), then it deletes it
+    } else {
+      setInvites((prev) => [...prev, id]); // otherwise we add it to our array(invites)
+    }
+  };
+
+  const onCLickSendInvites = () => {
+    setSucces(true);
+  };
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {success ? (
+        <Success count={invites.length} />
+      ) : (
+        <Users
+          onChangeSearchValue={onChangeSearchValue}
+          searchValue={searchValue}
+          items={users}
+          isLoading={isLoading}
+          invites={invites}
+          onClickInvite={onClickInvite} // we send our onClickInvite function through a prop called onClickInvite
+          onCLickSendInvites={onCLickSendInvites}
+        />
+      )}
     </div>
   );
 }
